1293db7dedb8fbb5eee41bb842d75ce71329edf7
Jessica Mauvais <jessica@pmjm.com>
2018-02-05 07:03:48 -0800

    try different import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+// import PlotlyHist from 'plotly.js/lib/histogram';
+// import PlotlyHist2d from 'plotly.js/lib/histogram2d';
+// import PlotlyHist2dCon from 'plotly.js/lib/histogram2dcontour';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -13,19 +13,56 @@ export class ChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const trace1 = {
-      x: [1, 2, 3, 4],
-      y: [10, 15, 13, 17],
-      type: 'scatter'
-    };
+    // -------------------
+    // regular line chart
+    // -------------------
+    // const trace1 = {
+    //   x: [1, 2, 3, 4],
+    //   y: [10, 15, 13, 17],
+    //   type: 'scatter'
+    // };
 
-    const trace2 = {
-      x: [1, 2, 3, 4],
-      y: [16, 5, 11, 9],
-      type: 'scatter'
-    };
+    // const trace2 = {
+    //   x: [1, 2, 3, 4],
+    //   y: [16, 5, 11, 9],
+    //   type: 'scatter'
+    // };
 
-    const data = [trace1, trace2];
-    Plotly.newPlot('myPlotlyDiv', data);
-  }
+    // -------------------
+    // histogram chart
+    // -------------------
+    // const data = [trace1, trace2];
+  //   let x = [];
+  //   for (let i = 0; i < 500; i ++) {
+  //     x[i] = Math.random();
+  //   }
+  //
+  //   const trace = {
+  //     x: x,
+  //     type: 'histogram',
+  //   };
+  //   const data = [trace];
+  //   console.log('data ', data);
+  //   PlotlyHist.newPlot('myPlotlyDiv', data);
+  // }
+
+  // -------------------
+  // line and bar chart
+  // -------------------
+  var trace1 = {
+  x: [0, 1, 2, 3, 4, 5],
+  y: [1.5, 1, 1.3, 0.7, 0.8, 0.9],
+  type: 'scatter'
+};
+
+var trace2 = {
+  x: [0, 1, 2, 3, 4, 5],
+  y: [1, 0.5, 0.7, -1.2, 0.3, 0.4],
+  type: 'bar'
+};
+
+var data = [trace1, trace2];
+
+Plotly.newPlot('myPlotlyDiv', data);
+}
 }
